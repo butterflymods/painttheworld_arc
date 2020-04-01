79144d8b6804b5f9071258cdce2c80f70b34d6cb
Daniel Orr <orrdaniel73@gmail.com>
2020-04-01 22:58:57 +0100

    Initial commit

---
+++ src/main/java/io/github/butterflymods/painttheworld/PaintTheWorld.java
@@ -0,0 +1,39 @@
+package io.github.butterflymods.painttheworld;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(PaintTheWorld.MOD_ID)
+public class PaintTheWorld {
+    public static final String MOD_ID = "paint-the-world";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public PaintTheWorld() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
+}
